7dd650b9469ac38f2c5230d47578e5c903a7fa7f
monalijain <monalijain419@gmail.com>
2015-03-27 20:03:05 +0530

    Changed access from csv to DB

--- WFList.py
+++ WFList.py
@@ -1,64 +1,71 @@
 __author__ = 'MJ'
-#Function which gives walkforward list, given the price series data; Number of Days in Training Data; Number of Days in Reporting Data
-
+#Function which gives walkforward list, given the price series data; Number of Days in Training Data; Number of Days in Reporting Date
 import GlobalVariables as gv
 import csv
 from datetime import date
+from DB_DBUtils import *
+
 def CreateWFList(SamplePriceSeries,T,R):
-    iFile=open('C:/Program Files/MariaDB 10.0/data/'+gv.name_database+'/PriceSeries.csv','rb')
-    rq=csv.reader(iFile)
+    dbObject4 = DBUtils()
+    dbObject4.dbConnect()
+    query= "SELECT *, 1 FROM " + gv.priceSeriesTable +"'"
+    print "Executing Query",query
+    resultDates = dbObject4.dbQuery(query)
+    
     c=0
     Tcounter=0
     TrainingBegin=list()
     TrainingEnd=list()
 
-    for row in rq:
+    for date,time,price,count in resultDates:
         if(c==0):
-            TrainingBegin.append(row[0])
+            TrainingBegin.append(str(date))
             Tcounter+=1
             #print row[0]
         else:
-            if(row1[0]!=row[0]):
+            if(date1!=date):
                 Tcounter+=1
                 #print row[0]
                 if(Tcounter%(R)==1):
-                    TrainingBegin.append(row[0])
+                    TrainingBegin.append(str(date))
                 if(Tcounter==T):
-                    TrainingEnd.append(row[0])
+                    TrainingEnd.append(str(date))
                 if(Tcounter>T and (Tcounter-T)%R==0):
-                    TrainingEnd.append(row[0])
+                    TrainingEnd.append(str(date))
         c=c+1
-        row1=row
+        date1=date
 
-    iFile=open('C:/Program Files/MariaDB 10.0/data/'+gv.name_database+'/PriceSeries.csv','rb')
-    rq=csv.reader(iFile)
+    query= "SELECT *, 1 FROM " + gv.priceSeriesTable +"'"
+    print "Executing Query",query
+    resultDates = dbObject4.dbQuery(query)
+
     Rcounter=1
     ReportingBegin=list()
     ReportingEnd=list()
     c=0
     Rcount=0
-    for row in rq:
+    for date,time,price,count in resultDates:
         if(c!=0):
-            if(row1[0]!=row[0]):
+            if(date1!=date):
                 try:
-                    i = TrainingEnd.index(row1[0])
+                    i = TrainingEnd.index(str(date1))
                 except ValueError:
                     i = -1 # no match
 
                 if(i!=-1):
-                    ReportingBegin.append(row[0])
+                    ReportingBegin.append(str(date))
                     Rcount=1
                     Rcounter=0
 
                 if(Rcount==1):
                     Rcounter+=1
                 if(Rcounter==R):
-                    ReportingEnd.append(row[0])
+                    ReportingEnd.append(str(date))
                     Rcount=0
                     Rcounter=0
 
         c=c+1
-        row1=row
+        date1=date
 
     while(len(TrainingBegin)!= len(ReportingEnd)):
         TrainingBegin.pop()
